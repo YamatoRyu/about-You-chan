d8a87752f003db331de979e381b1ae8cc3712108
YamatoRyuma <gamerliu2004@gmail.com>
2025-08-09 19:48:46 +0700

    About You-chan part 2

--- src/Home.jsx
+++ src/Home.jsx
@@ -43,6 +43,26 @@ function Home() {
             </nav>
 
             <h1 className="you-chan_title">Watanabe You (渡辺 曜)</h1>
+
+            <div className="about">
+                <p>
+                    <span style={{color: '#66c0ff'}}>Watanabe You</span> is a main character in Love Live! Sunshine!!.
+                    She is a second-year student at Numazu High School. She is a member of <span
+                    style={{color: '#ff9547'}}>CYaRon!</span> and <span style={{color: '#CBEFFF'}}>YYY</span>,
+                    two sub-units under <span style={{color: '#009fe8'}}>Aqours</span>. Her image color is <span
+                    style={{color: '#add8e6'}}>light blue</span>. <br/><br/>
+
+                    She was a second-year student at both Uranohoshi Girls' High School prior to its closure and
+                    Seishin High School (albeit temporarily) before fully integrated into Numazu High.
+                </p>
+            </div>
+
+            <hr style={{marginTop: '50px', marginBottom: '20px'}} />
+
+            <h2 className="statistics">Statistics</h2>
+
+            <div className="school_name">
+            </div>
         </>
     );
 }
